891fc736e2aa7805aba08c1da37d96ab0a4a75a1
ranman <liuranman@gmail.com>
2014-03-09 23:37:44 +0800

    Initialise the application with rails 4.0.0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SecurityChallenge::Application.config.secret_key_base = 'a80d4f286f9aeaab1b1e8e4b03e5fd50b2b91781c532586df2ea77ac0d5a13fd394ed0bc5eab4663edb4a7f5f5b917ad429bd150ae837ce3a4f795a25450ef00'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SecurityChallenge::Application.config.session_store :cookie_store, key: '_SecurityChallenge_session'
