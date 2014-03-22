67375b8569f29784d099ac4ad141fc8498df651f
ranman <liuranman@gmail.com>
2014-03-22 16:00:13 +0800

    [ran]implement the score board page

--- app/controllers/score_controller.rb
+++ app/controllers/score_controller.rb
@@ -1,4 +1,23 @@
 class ScoreController < ApplicationController
   def show
+
+    if ! params['commit'].nil?
+      @challenge_index = Challenges.find_by_answer_token(params['answer_token'])
+      if ! @challenge_index.nil?
+        begin
+          Scores.find_or_create_by!(user_name: params['your_name'], challenge_index: @challenge_index['index'])
+        rescue
+          printf("exception");
+        end
+      end
+    end
+
+    @challenges = Challenges.order("'index'").all
+
+    @scores = Array.new
+    (0..4).each do |i|
+      @scores[i] = Scores.where("challenge_index=?",i+1).order("created_at").all
+    end
+
   end
 end
--- app/models/scores.rb
+++ app/models/scores.rb
@@ -1,2 +1,4 @@
 class Scores < ActiveRecord::Base
+  validates :user_name, length: { in: 2..20 }
+  validates :challenge_index, length: { in:1..2}
 end
