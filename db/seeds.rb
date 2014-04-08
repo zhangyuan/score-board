26a8e95f375df95701158b10ff70ccc3afa31e0c
ranman <liuranman@gmail.com>
2014-04-08 16:11:06 +0800

    [Ran]modify the http port for production and change the answer token

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-challenges = Challenges.create([{index: '1', title: 'Challenge 1',answer_token: 'abcdef1'}])
-challenges = Challenges.create([{index: '2', title: 'Challenge 2',answer_token: 'abcdef2'}])
-challenges = Challenges.create([{index: '3', title: 'Challenge 3',answer_token: 'abcdef3'}])
-challenges = Challenges.create([{index: '4', title: 'Challenge 4',answer_token: 'abcdef4'}])
-challenges = Challenges.create([{index: '5', title: 'Challenge 5',answer_token: 'abcdef5'}])
+challenges = Challenges.create([{index: '1', title: 'Challenge 1',answer_token: '8A63DA12D92E639F'}])
+challenges = Challenges.create([{index: '2', title: 'Challenge 2',answer_token: '676D9A04B99F1673'}])
+challenges = Challenges.create([{index: '3', title: 'Challenge 3',answer_token: '757B5DEE77D50012'}])
+challenges = Challenges.create([{index: '4', title: 'Challenge 4',answer_token: 'BA9CC28D07D19979'}])
